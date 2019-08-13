942279a078b300f6d0d0b3b0a87ca0f46427ef28
junscapital <52300722+junscapital@users.noreply.github.com>
2019-08-14 00:55:27 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -2,6 +2,8 @@
 cd /home/ubuntu/nyzoVerifier
 sudo git reset --hard
 sudo git pull origin master
+sudo git fetch
+sudo git checkout 01c04f81d5732dd998ca38f27c60e908aab95c06
 sudo ./gradlew build
 sudo supervisorctl reload
 sudo renice -n -5 -p $(pgrep ^java$)
