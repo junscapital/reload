d314187271767f5846ca65b6f51db23839d82d8e
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-08 09:07:06 +0800

    Update verifier.sh

--- verifier.sh
+++ verifier.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
-sudo renice -n -5 -p $(pgrep ^java$)
+
