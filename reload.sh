230b64f18b68964ae20435cc70df54af79a0c97a
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-25 02:06:43 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -9,11 +9,5 @@ rm myverifier
 sudo ufw enable
 sudo ufw allow 22
 sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo git fetch
-sudo git checkout 9e66c00c98ce75b0df4b565ecbd163e65caef57a
-sudo ./gradlew build
 sudo supervisorctl reload
 sudo renice -n -5 -p $(pgrep ^java$)
