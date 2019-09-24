f9882e9b14f0906551088103882e3f26a7046038
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-25 02:00:40 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
+1 1,13 * * * sudo ufw allow 9444/tcp
+11 1,13 * * * sudo ufw deny 9444/tcp
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
 sudo ufw enable
+sudo ufw allow 22
+sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
 cd /home/ubuntu/nyzoVerifier
 sudo git reset --hard
 sudo git pull origin master
