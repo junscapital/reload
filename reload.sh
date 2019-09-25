9603e22903d9ea8d25a8fec93f3f419e9efef97c
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-25 15:15:11 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -10,8 +10,8 @@ sudo supervisorctl reload
 sudo renice -n -5 -p $(pgrep ^java$)
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-1 1,13 * * * sudo ufw allow 9444/tcp && sudo ufw reload
-11 1,13 * * * sudo ufw deny 9444/tcp && sudo ufw reload
+2 1,13 * * * sudo ufw allow 9444/tcp && sudo ufw reload
+12 1,13 * * * sudo ufw deny 9444/tcp && sudo ufw reload
  " >> myverifier
 sudo crontab myverifier
 rm myverifier
