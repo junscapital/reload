d87f72ea036c6c3ae1616c29f355c914ade8cb26
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-25 10:10:12 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -3,9 +3,10 @@ sudo su
 cd /home/ubuntu/nyzoVerifier
 sudo git reset --hard
 sudo git pull origin master
+sudo git fetch
+sudo git checkout 9e66c00c98ce75b0df4b565ecbd163e65caef57a
 sudo ./gradlew build
 sudo supervisorctl reload
-sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1 1,13 * * * sudo ufw allow 9444/tcp && sudo ufw reload
