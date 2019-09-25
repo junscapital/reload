376e8aac9b26ed5d28ec09426ae86e089416febc
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-25 09:58:54 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -7,7 +7,6 @@ sudo ./gradlew build
 sudo supervisorctl reload
 sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
-sudo ufw deny 9444/tcp && sudo ufw reload
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1 1,13 * * * sudo ufw allow 9444/tcp && sudo ufw reload
 11 1,13 * * * sudo ufw deny 9444/tcp && sudo ufw reload
@@ -15,6 +14,5 @@ echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/r
 sudo crontab myverifier
 rm myverifier
 sudo ufw enable
-sudo ufw allow 22
-sudo ufw reload
+sudo ufw allow 22 && sudo ufw reload
 sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
