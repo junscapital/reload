accb0a093436d013d356e318c3fdc990c15a4ec8
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-25 09:31:51 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sudo su
+cd /home/ubuntu/nyzoVerifier
+sudo git reset --hard
+sudo git pull origin master
+sudo ./gradlew build
+sudo supervisorctl reload
+sleep 3s
+sudo renice -n -5 -p $(pgrep ^java$)
 sudo ufw deny 9444/tcp && sudo ufw reload
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1 1,13 * * * sudo ufw allow 9444/tcp && sudo ufw reload
@@ -10,5 +17,3 @@ rm myverifier
 sudo ufw enable
 sudo ufw allow 22
 sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
-sudo supervisorctl reload
-sudo renice -n -5 -p $(pgrep ^java$)
