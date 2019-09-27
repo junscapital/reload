7634687ef4bd75bdcb557cc249f3add163b48a1a
junscapital <52300722+junscapital@users.noreply.github.com>
2019-09-27 08:24:16 +0800

    Update everyday.sh

--- everyday.sh
+++ everyday.sh
@@ -1,1 +1,9 @@
-
+#!/bin/sh
+cd /home/ubuntu/nyzoVerifier
+sudo git reset --hard
+sudo git pull origin master
+sudo git fetch
+sudo git checkout 9e66c00c98ce75b0df4b565ecbd163e65caef57a
+sudo ./gradlew build
+sudo supervisorctl reload
+sudo renice -n -5 -p $(pgrep ^java$)
