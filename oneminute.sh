2341b3e2f8d059e56c7eb808cfded55ca7fa7338
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-03 07:58:31 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,12 +1,1 @@
 #!/bin/sh
-sudo su
-sudo apt clean
-sudo rm -rf ~/.cache/thumbnails/*
-sudo ufw allow 9444/tcp
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
