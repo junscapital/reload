431d4571d77143eff5ae2556c9e99bc295b3dd65
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-19 14:49:50 +0800

    Update try.sh

--- try.sh
+++ try.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo ufw enable
-y
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/NKN/master/everyhour | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
+cd /home/ubuntu
+wget -O install.sh 'https://api.nknx.org/deployment/install/e55606c6-c114-4ced-9529-5f43fb181320'
+sudo bash install.sh
