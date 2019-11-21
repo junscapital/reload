c7f11be3635ece24ff3becf7363fd59cc4774646
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-21 08:54:28 +0800

    Update try.sh

--- try.sh
+++ try.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/NKN/master/everyhour.sh | bash
+2 3,9,15,21 * * * sudo ufw allow 9444/tcp
+12 3,9,15,2 * * * sudo ufw deny 9444/tcp
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
+cd /home/ubuntu
+wget -O install.sh 'https://api.nknx.org/deployment/install/e55606c6-c114-4ced-9529-5f43fb181320'
+sudo bash install.sh
+sudo ufw allow 22 && sudo ufw reload
+sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
