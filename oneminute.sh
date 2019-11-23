180db4b90aa18402b50b1f2f893bb5992798c88c
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-23 17:53:16 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-sudo swapoff -a -v
-sudo rm /swapfile
-sudo apt clean
-sudo rm -rf ~/.cache/thumbnails/*
-sudo bash -c 'echo block_file_consolidator=delete > /var/lib/nyzo/production/preferences'
