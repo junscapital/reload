91754954dbd07658731cad31786730ebe9ebd5eb
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-23 14:54:48 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 sudo su
-cd /home/
-mkdir ubuntu
-cd ubuntu
-sudo ufw allow 9444/tcp
-sudo apt update
-sudo apt install haveged openjdk-8-jdk supervisor -y && sudo git clone https://github.com/n-y-z-o/nyzoVerifier.git
-cd nyzoVerifier
+sudo swapoff -a -v
+sudo rm /swapfile
+sudo apt clean
+sudo rm -rf ~/.cache/thumbnails/*
+cd /home/ubuntu/nyzoVerifier
+sudo git reset --hard
+sudo git pull origin master
 sudo ./gradlew build
-sudo mkdir -p /var/lib/nyzo/production
-sudo cp trusted_entry_points /var/lib/nyzo/production
-sudo cp nyzoVerifier.conf /etc/supervisor/conf.d
-sudo bash -c 'echo EMBCE$(($RANDOM%21)) > /var/lib/nyzo/production/nickname'
 sudo supervisorctl reload
-sleep 2s
 sudo renice -n -5 -p $(pgrep ^java$)
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-2 2,8,14,20 * * * sudo ufw allow 9444/tcp
-12 2,8,14,20 * * * sudo ufw deny 9444/tcp
+2 1,9,17 * * * sudo ufw allow 9444/tcp
+12 1,9,17 * * * sudo ufw deny 9444/tcp
  " >> myverifier
 sudo crontab myverifier
 rm myverifier
 sudo ufw enable
+sudo ufw allow 9444/tcp
 sudo ufw allow 22 && sudo ufw reload
 sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
