dbe9b1a4e05adc488e964764f7cb85d57e6b023a
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-25 21:35:08 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-sudo sed -i '1i\always_track_blockchain=1' /var/lib/nyzo/production/preferences
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
-sudo supervisorctl status
