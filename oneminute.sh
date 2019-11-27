a35d2df41fc488dfa50a6104dffbccd5b5670ed4
junscapital <52300722+junscapital@users.noreply.github.com>
2019-11-27 20:43:33 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-sudo su
-sudo swapoff -a -v
-sudo rm /swapfile
-sudo apt clean
-sudo rm -rf ~/.cache/thumbnails/*
-cd /var/lib/nyzo/production/blocks
-sudo rm -rf 002 003 004 005
-sudo rm -rf /var/lib/nyzo/production/blocks/individual
-sudo mkdir /var/lib/nyzo/production/blocks/individual/
-sudo ufw allow 9444/tcp
-sudo bash -c 'echo block_file_consolidator=delete > /var/lib/nyzo/production/preferences'
-sudo sed -i '1i\always_track_blockchain=1' /var/lib/nyzo/production/preferences
+sudo ufw deny 9444/tcp
