dc2974a33111a21bcfb9c52f1f579da8b884b767
junscapital <52300722+junscapital@users.noreply.github.com>
2019-12-06 13:40:09 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -4,20 +4,5 @@ sudo swapoff -a -v
 sudo rm /swapfile
 sudo apt clean
 sudo rm -rf ~/.cache/thumbnails/*
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
 sudo supervisorctl reload
 sudo renice -n -5 -p $(pgrep ^java$)
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-2 1,9,17 * * * sudo ufw allow 9444/tcp
-12 1,9,17 * * * sudo ufw deny 9444/tcp
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
-sudo ufw enable
-sudo ufw allow 9444/tcp
-sudo ufw allow 22 && sudo ufw reload
-sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
