2fedd2ae3d9a5bc2373b1d436fa209bb89e11c34
junscapital <52300722+junscapital@users.noreply.github.com>
2019-12-10 11:05:14 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -4,10 +4,9 @@ sudo apt clean
 sudo rm -rf ~/.cache/thumbnails/*
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-2 1,4,7,10,13,16,19,22 sudo apt clean && sudo rm -rf ~/.cache/thumbnails/*
+2 1,4,7,10,13,16,19,22 * * * sudo apt clean && sudo rm -rf ~/.cache/thumbnails/*
 3 1,4,7,10,13,16,19,22 * * * sudo ufw allow 9444/tcp
 8 1,4,7,10,13,16,19,22 * * * sudo ufw deny 9444/tcp
  " >> myverifier
 sudo crontab myverifier
 rm myverifier
-
