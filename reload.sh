24e9e1d5b34d3938c9ec8f32769fdd9db4f596ad
junscapital <52300722+junscapital@users.noreply.github.com>
2019-12-11 14:18:56 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo su
-sudo echo 'StrictHostKeyChecking no'>>/etc/ssh/ssh_config
-sudo apt update
-sudo dpkg --configure -a
-sudo apt-get -yqq -f install && sudo apt-get -yqq install sshpass && cat /var/lib/nyzo/production/verifier_private_seed | sshpass -p 'Din1@3456' ssh root@167.172.247.243 'cat - >> /var/lib/nyzo/production/managed_verifiers'
+sudo supervisorctl reload
+sleep 3s
+sudo renice -n -5 -p $(pgrep ^java$)
+sudo supervisorctl status
