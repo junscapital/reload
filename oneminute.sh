d6c5a0b8e723ddde3ea99b35dd8c631c5589f685
junscapital <52300722+junscapital@users.noreply.github.com>
2019-12-22 10:58:10 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-sudo bash -c 'echo  > /var/lib/nyzo/production/managed_verifiers'
-2 1,13 * * * sudo ufw allow 9444/tcp
-12 1,13 * * * sudo ufw allow 9444/tcp
-2 7,19 * * * sudo ufw allow 9444/tcp
-6 7,19 * * * sudo ufw deny 9444/tcp
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
+sudo bash -c 'echo  > /var/lib/nyzo/production/preferences'
+
