6afd25d86b9a4adadab70520a9f744fa7aef8b50
junscapital <52300722+junscapital@users.noreply.github.com>
2019-12-22 12:13:48 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,11 +1,1 @@
 #!/bin/sh
-sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-2 1,13 * * * sudo ufw allow 9444/tcp
-22 1,13 * * * sudo ufw allow 9444/tcp
-2 7,19 * * * sudo ufw allow 9444/tcp
-5 7,19 * * * sudo ufw deny 9444/tcp
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
