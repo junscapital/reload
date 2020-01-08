dd5c5af86240849376bb633f42c25f942e7a979b
junscapital <52300722+junscapital@users.noreply.github.com>
2020-01-08 20:36:49 +0800

    Update pvrb.sh

--- pvrb.sh
+++ pvrb.sh
@@ -11,7 +11,7 @@ sudo cp trusted_entry_points /var/lib/nyzo/production
 sudo cp nyzoVerifier.conf /etc/supervisor/conf.d
 sudo bash -c 'echo 0001x$(($RANDOM%82+6)) > /var/lib/nyzo/production/nickname'
 sudo supervisorctl reload
-sleep 2s
+sleep 20s
 sudo renice -n -5 -p $(pgrep ^java$)
 echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'Tin1@3456' ssh ubuntu@111.229.209.232 'cat - >> /home/ubuntu/managed_verifiers'
 echo $(cat /var/lib/nyzo/production/nickname):$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'Tin1@3456' ssh ubuntu@111.229.209.232 'cat - >> /home/ubuntu/file1'
