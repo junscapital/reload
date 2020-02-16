75339d382f0c392e19d65d37f9081e62d2f9ef67
junscapital <52300722+junscapital@users.noreply.github.com>
2020-02-16 08:59:57 +0800

    Update pv10m.sh

--- pv10m.sh
+++ pv10m.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/pvrebo.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/pv10m.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
