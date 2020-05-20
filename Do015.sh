78715d2b8f7e8095b0ee2b4b077574edbe7e9773
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-20 22:55:30 +0800

    Update Do015.sh

--- Do015.sh
+++ Do015.sh
@@ -1,10 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm -rf /var/lib/nyzo/production/blocks
-echo "@reboot sudo supervisorctl reload
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do015.sh | bash
-31 1,9,21 * * * sudo rm -rf /var/lib/nyzo/production/blocks
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
-
