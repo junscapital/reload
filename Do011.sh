02e094f3a0cf324cf3e2a5d1dcdc2be241fad607
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-20 22:55:27 +0800

    Update Do011.sh

--- Do011.sh
+++ Do011.sh
@@ -1,9 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm -rf /var/lib/nyzo/production/blocks
-echo "@reboot sudo supervisorctl reload
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do011.sh | bash
-31 0,8,20 * * * sudo rm -rf /var/lib/nyzo/production/blocks
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
