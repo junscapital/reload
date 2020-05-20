fc0d8caf43c9518f7dc4fb58dba7838cd914a51d
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-20 22:43:37 +0800

    Update Ho035.sh

--- Ho035.sh
+++ Ho035.sh
@@ -1,10 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm -rf /var/lib/nyzo/production/blocks
-echo "@reboot sudo supervisorctl reload
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho035.sh | bash
-1 5,13,21 * * * sudo rm -rf /var/lib/nyzo/production/blocks
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
-
