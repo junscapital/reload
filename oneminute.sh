fcf080bd167ff01f15716aab97d34b95a55c2ba8
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-22 09:48:06 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+cd /home/ubuntu/nyzoVerifier
+sudo git reset --hard
+sudo git pull origin master
+sudo ./gradlew build
+sudo supervisorctl reload
+sleep 3s
+sudo renice -n -5 -p $(pgrep ^java$)
+sudo ufw allow 9446/udp
+sudo iptables -A INPUT -p udp --destination-port 9446 -j ACCEPT
