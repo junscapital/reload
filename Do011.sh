9e0c2b428228a28a799bf38b78450346ad30d5c1
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-23 23:54:34 +0800

    Update Do011.sh

--- Do011.sh
+++ Do011.sh
@@ -2,7 +2,8 @@
 cd /home/ubuntu/nyzoVerifier
 sudo git reset --hard
 sudo git pull origin master
+sudo git fetch
+sudo git checkout da9f8ae84af806a9f69758e1244829dd2ec62c9c
 sudo ./gradlew build
 sudo supervisorctl reload
-sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
