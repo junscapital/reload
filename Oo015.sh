e86d04e1ad6f5c72e9feaf680bcac2653cedb194
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-24 23:48:00 +0800

    Update Oo015.sh

--- Oo015.sh
+++ Oo015.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Oo015.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
 
