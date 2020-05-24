9def048e8db8f75297c34565260db8201f0868b4
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-24 23:28:00 +0800

    Update Do0150.sh

--- Do0150.sh
+++ Do0150.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do0150.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
