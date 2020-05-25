59813b6ae861bdfe0ca2512d11ddf4415ab6c78f
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-26 01:42:30 +0800

    Update Oo015.sh

--- Oo015.sh
+++ Oo015.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-sudo rm -rf /var/lib/nyzo/production/blocks/individual/
-sudo mkdir /var/lib/nyzo/production/blocks/individual/
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
