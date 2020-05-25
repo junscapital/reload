39d20fd527120c2d277300c362e52452a22657b0
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-25 22:43:15 +0800

    Update senquehz.sh

--- senquehz.sh
+++ senquehz.sh
@@ -1,1 +1,9 @@
-
+#!/bin/sh
+sudo su
+sudo rm -rf /var/lib/nyzo/production/blocks
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/senquehz.sh | bash
+55 2,10,12 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
