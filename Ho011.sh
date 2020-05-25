f267cb2e7f67b4432ca5dd1b0a5b9aebf8a736d2
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-26 00:53:32 +0800

    Update Ho011.sh

--- Ho011.sh
+++ Ho011.sh
@@ -1,10 +1,1 @@
 #!/bin/sh
-sudo rm -rf /var/lib/nyzo/production/blocks
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
-sudo supervisorctl status
