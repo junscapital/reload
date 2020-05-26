dcae41b7473dfddf2299af3e0606f0bc04d7165e
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-26 23:59:15 +0800

    Update Oo015.sh

--- Oo015.sh
+++ Oo015.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Oo015.sh | bash
+2 1,9,17 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
