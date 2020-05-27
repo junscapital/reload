e43ae606764ce70958495ae7499b7f6a21950b73
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-27 15:32:27 +0800

    Update Do0150.sh

--- Do0150.sh
+++ Do0150.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+sudo su
+sudo rm -rf /var/lib/nyzo/production/blocks
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do0150.sh | bash
+1 4,12,20 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
