1f7d53efbd93616333af85dbb77264043a3bb9d6
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-27 08:16:35 +0800

    Update Vo015.sh

--- Vo015.sh
+++ Vo015.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+sudo su
+echo "@reboot sudo supervisorctl reload
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Vo015.sh | bash
+1 2,10,18 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
