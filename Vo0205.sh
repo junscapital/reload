6ddc68c72ed38974c0ee70e793fdc47194b98e69
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-28 11:37:54 +0800

    Update Vo0205.sh

--- Vo0205.sh
+++ Vo0205.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Vo0205.sh | bash
+1 1,13 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
