17a6c69f5a3d916a164bf4126469c60bf9bf2ed7
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-28 17:15:41 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-sudo su
-sudo rm -rf /var/lib/nyzo/production/blocks/
-sudo rm -rf /var/lib/nyzo/production/blocks/
-mkdir /var/lib/nyzo/production/blocks/individual/
-cd /var/lib/nyzo/production/blocks/individual/
-wget https://seed.nyzo.co/seedTransactions/genesis
 sudo ufw allow 9444/tcp
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
