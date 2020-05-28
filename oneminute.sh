f1e0968e699ea5e9ef4e817acef326aef3a4a00c
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-28 17:03:16 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-sudo ufw deny 9444/tcp
+sudo su
+sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo rm -rf /var/lib/nyzo/production/blocks/
+mkdir /var/lib/nyzo/production/blocks/individual/
+cd /var/lib/nyzo/production/blocks/individual/
+wget https://seed.nyzo.co/seedTransactions/genesis
+sudo ufw allow 9444/tcp
 cd /home/ubuntu/nyzoVerifier
 sudo git reset --hard
 sudo git pull origin master
 sudo ./gradlew build
 sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
-sudo supervisorctl status
