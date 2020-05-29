e3d380fd33215c01e06dd6c2806ef32307982a0d
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-29 13:02:33 +0800

    Update Ho011.sh

--- Ho011.sh
+++ Ho011.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+sudo su
+cd /root/
+wget https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh 
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho011.sh | bash
+1 8,20 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+*/1 * * * * sudo bash /root/blockip.sh
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
