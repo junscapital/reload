26d004753e88e6864727f41d3f90cc95d9ed79cf
junscapital <52300722+junscapital@users.noreply.github.com>
2020-05-30 18:23:59 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+sudo rm -rf ~/.cache/thumbnails/*
+sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo ufw allow 9444/tcp
+sudo supervisorctl reload
