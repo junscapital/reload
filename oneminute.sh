b39a09937b038b2c413e2e841ad8dcab8b6b5bcb
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-01 12:12:14 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,8 +1,1 @@
 #!/bin/sh
-sudo rm -rf /var/lib/nyzo/production/blocks/
-sudo rm -rf /var/lib/nyzo/production/blocks/
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
