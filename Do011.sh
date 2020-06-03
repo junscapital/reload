30a34d5b26b53068d35e8d593fe5203a025f07aa
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-03 17:03:23 +0800

    Update Do011.sh

--- Do011.sh
+++ Do011.sh
@@ -1,8 +1,1 @@
 #!/bin/sh
-sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do011.sh | bash
-*/1 * * * * sudo bash /root/blockip.sh
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
