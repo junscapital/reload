5774de02fd577acf9d459feeaf11eb96a07f0497
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-03 16:52:30 +0800

    Update Do015.sh

--- Do015.sh
+++ Do015.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do015.sh | bash
+*/1 * * * * sudo bash /root/blockip.sh
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
