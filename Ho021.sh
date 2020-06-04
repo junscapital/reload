7daa634ecddaa6986f6f5ade8625fea3bd2fd1af
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-04 11:23:50 +0800

    Update Ho021.sh

--- Ho021.sh
+++ Ho021.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
 sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
