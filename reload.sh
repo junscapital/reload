6eacd3173ba18400d8cd5f842cdc28ba073b7cc0
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-09 00:08:40 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #sudo su
 #echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'OwRXBiZ0' ssh root@51.75.249.70 'cat - >> /root/managed_verifiers'
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
 sudo supervisorctl reload
 sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
