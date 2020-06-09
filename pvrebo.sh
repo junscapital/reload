e5f4b97e04b228c639144025115cd47534adaa3a
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-09 11:15:52 +0800

    Update pvrebo.sh

--- pvrebo.sh
+++ pvrebo.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 sudo sed -i "s/200M/300M/g"  /etc/supervisor/conf.d/nyzoVerifier.conf
+cd /home/ubuntu/nyzoVerifier
+sudo git reset --hard
+sudo git pull origin master
+sudo ./gradlew build
+sudo supervisorctl reload
