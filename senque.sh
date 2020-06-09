df578220858b9e295ec2df904df4f6966c462c57
junscapital <52300722+junscapital@users.noreply.github.com>
2020-06-09 16:04:21 +0800

    Update senque.sh

--- senque.sh
+++ senque.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+echo "@reboot sudo supervisorctl reload
+5 0,3,6,9,12,15,18,21 * * * sudo rm -rf /var/lib/nyzo/production/blocks
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/senque.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
