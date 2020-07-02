69a504ed1c038f6341ea2c4331b9d4f8d4dbec1d
junscapital <52300722+junscapital@users.noreply.github.com>
2020-07-02 11:29:00 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-#sudo su
-
-#sudo ifdown ens5
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
+10 3,11,19 * * *  sudo ifup ens5 && sleep 30s && sudo rm -rf ~/.cache/thumbnails/* && sudo rm -rf /var/lib/nyzo/production/blocks && sudo supervisorctl reload
+43 3,11,19 * * * sudo ifdown ens5
+55 15 * * * sudo reboot
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
+sudo ifdown ens5
