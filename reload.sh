b367aac5e39b223f8dc0b951bb44efec97776096
junscapital <52300722+junscapital@users.noreply.github.com>
2020-07-24 21:52:07 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
+5 0,8,16 * * *  sudo ufw allow 9444/tcp && sleep 30s && sudo rm -rf ~/.cache/thumbnails/* && sudo rm -rf /var/lib/nyzo/production/blocks && sudo supervisorctl reload
+38 0,8,16 * * * sudo ufw deny 9444/tcp
+55 15 * * * sudo reboot
+ " >> myverifier
 sudo supervisorctl reload
 sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
-sleep 120s
-sudo ifdown ens5
+#sleep 120s
+#sudo ifdown ens5
