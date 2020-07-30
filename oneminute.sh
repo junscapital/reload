f39287bb690c82b6d1d3873e6e5604ab936da189
junscapital <52300722+junscapital@users.noreply.github.com>
2020-07-30 11:49:17 +0800

    Update oneminute.sh

--- oneminute.sh
+++ oneminute.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-#sudo su
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
+55 15 * * * sudo reboot
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
 #sleep 600s
 #sudo ifdown ens5
