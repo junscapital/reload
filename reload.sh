17963f9a3070f7fe8599eaff9c34dc616d444652
junscapital <52300722+junscapital@users.noreply.github.com>
2020-07-30 15:00:52 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo su
+#sudo su
 #echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'OwRXBiZ0' ssh root@51.75.249.70 'cat - >> /root/managed_verifiers'
 #sudo ufw allow 9444/tcp
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/testreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/testoneminute.sh | bash
-1 0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17,18,19,20,21,22,23 * * * sudo ifup ens5 
-31 0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17,18,19,20,21,22,23 * * * sudo ifdown ens5
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
+#echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
+#1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
+#1 0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17,18,19,20,21,22,23 * * * sudo ifup ens5 
+#31 0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17,18,19,20,21,22,23 * * * sudo ifdown ens5
+# " >> myverifier
+#sudo crontab myverifier
+#rm myverifier
 sudo supervisorctl reload
 sleep 3s
 sudo renice -n -5 -p $(pgrep ^java$)
