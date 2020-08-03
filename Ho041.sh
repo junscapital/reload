7aa1a53d38e56203935b7285437dca76fcf79519
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-03 17:43:23 +0800

    Update Ho041.sh

--- Ho041.sh
+++ Ho041.sh
@@ -1,11 +1,1 @@
 #!/bin/sh
-rm -rf /home/ubuntu/nyzoVerifier 
-cd /home/ubuntu/
-sudo git clone https://github.com/n-y-z-o/nyzoVerifier.git
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo git fetch
-sudo git checkout 75786060a822443154bcdaaa371fe8696d54a201
-sudo ./gradlew build
-sudo supervisorctl reload 
