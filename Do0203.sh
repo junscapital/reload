098fe2420778e1e0b2cf168db6598a3459a63034
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-06 22:58:08 +0800

    Update Do0203.sh

--- Do0203.sh
+++ Do0203.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+sudo su
+sudo rm /home/blockip.sh
+sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do0203.sh | bash
+*/1 * * * * sudo bash /home/blockip.sh
+3 2 * * 1,4 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
+sudo supervisorctl reload
