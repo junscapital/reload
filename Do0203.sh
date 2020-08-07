89c6c0bc1ea0925707974ff6e1bcdb87d526f3f1
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-07 13:34:28 +0800

    Update Do0203.sh

--- Do0203.sh
+++ Do0203.sh
@@ -1,11 +1,1 @@
 #!/bin/sh
-sudo rm /var/lib/nyzo/production/nodes
-sudo wget -P /var/lib/nyzo/production/ https://raw.githubusercontent.com/junscapital/reload/master/nodes
-sudo rm /home/blockip.sh
-sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
-sudo bash /home/blockip.sh
-cd /home/ubuntu/nyzoVerifier 
-sudo git fetch origin pull/25/head:NCFP10
-sudo git checkout NCFP10
-sudo ./gradlew build
-sudo supervisorctl reload 
