db91224972d8e52f373fac2894aab163e23222b0
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-10 19:29:25 +0800

    Update blockip.sh

--- blockip.sh
+++ blockip.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo sed -i -e '/66.70.231/d'  /var/lib/nyzo/production/nodes
 sudo sed -i -e '/95.216.121/d'  /var/lib/nyzo/production/nodes
 sudo sed -i -e '/142.44.145/d'  /var/lib/nyzo/production/nodes
 sudo sed -i -e '/51.77.7/d'  /var/lib/nyzo/production/nodes
