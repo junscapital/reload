b59c17e133c8bbc2e0bba83a5801e2f6cdde7417
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-11 00:43:21 +0800

    Update Ho041.sh

--- Ho041.sh
+++ Ho041.sh
@@ -1,7 +1,1 @@
 #!/bin/sh
-sudo rm /home/blockip.sh
-sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
-sudo bash /home/blockip.sh
-cd /home/ubuntu/nyzoVerifier
-sudo ./gradlew build
-sudo supervisorctl reload 
