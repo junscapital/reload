8e447f9dfa1fa603834d7577efbcd10f937d6436
junscapital <52300722+junscapital@users.noreply.github.com>
2020-08-12 06:40:36 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -2,9 +2,9 @@
 sudo su
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
-46 0,8,16 * * *  sudo ifup ens5
-46 4,12,20 * * * sudo ifdown ens5
-6 17 * * 2  sudo rm -rf ~/.cache/thumbnails/* && sudo rm -rf /var/lib/nyzo/production/blocks && sudo supervisorctl reload
+28 0,8,16 * * *  sudo ifup ens5
+28 4,12,20 * * * sudo ifdown ens5
+48 8 * * 1  sudo rm -rf ~/.cache/thumbnails/* && sudo rm -rf /var/lib/nyzo/production/blocks && sudo supervisorctl reload
  " >> myverifier
 sudo crontab myverifier
 rm myverifier
