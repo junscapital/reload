11f6b2dae9bd99f7d8c63ac2bff9d271be4ed00e
junscapital <52300722+junscapital@users.noreply.github.com>
2020-09-06 17:54:10 +0800

    Update Ho015.sh

--- Ho015.sh
+++ Ho015.sh
@@ -1,12 +1,1 @@
 #!/bin/sh
-#!/bin/sh
-sudo su
-sudo rm -rf /var/lib/nyzo/production/blocks/
-sudo rm -rf /var/lib/nyzo/production/blocks/
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho015.sh | bash
-*/1 * * * * sudo bash /home/blockip.sh
-15 1 * * 1,4 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
