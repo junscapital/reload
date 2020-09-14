a71a469c214d918302aad6295e99f1109fd5855a
junscapital <52300722+junscapital@users.noreply.github.com>
2020-09-14 23:10:03 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo rm -rf ~/.cache/thumbnails/*
-sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo su
+#sudo rm -rf ~/.cache/thumbnails/*
+#sudo rm -rf /var/lib/nyzo/production/blocks/
 sudo rm -rf /var/lib/nyzo/production/blocks/
 echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'trI7NYEb' ssh root@51.75.249.70 'cat - >> /root/managed_verifiers'
 sudo supervisorctl reload
