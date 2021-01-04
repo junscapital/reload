877c44de24d3248012fc2023501a0a48810f02a8
junscapital <52300722+junscapital@users.noreply.github.com>
2021-01-04 19:53:56 +0800

    Update Ho025.sh

--- Ho025.sh
+++ Ho025.sh
@@ -1,12 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm /home/blockip.sh
-sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
-sudo bash /home/blockip.sh
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
