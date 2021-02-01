4fce0905aa5ac8db502db673f0947816160ea527
junscapital <52300722+junscapital@users.noreply.github.com>
2021-02-01 18:34:28 +0800

    Update Ho035.sh

--- Ho035.sh
+++ Ho035.sh
@@ -1,9 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm /var/lib/nyzo/production/nodes
-sudo wget -P /var/lib/nyzo/production/ https://raw.githubusercontent.com/junscapital/reload/master/nodes
-cd /home/ubuntu/nyzoVerifier
-sudo ./gradlew build
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
