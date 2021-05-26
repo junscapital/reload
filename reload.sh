3d61d0cb3d157630cac65a0b1c7554eb32dea3f6
junscapital <52300722+junscapital@users.noreply.github.com>
2021-05-26 13:04:10 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -5,4 +5,11 @@
 #sudo git reset --hard
 #sudo git pull origin master
 #sudo ./gradlew build
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
+21 12 * * 5  sudo rm -rf ~/.cache/thumbnails/* && sudo rm -rf /var/lib/nyzo/production/blocks && sudo supervisorctl reload
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
 sudo supervisorctl reload
