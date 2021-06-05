076e49ce386db628970bc2325c275a63f6f26825
junscapital <52300722+junscapital@users.noreply.github.com>
2021-06-05 10:12:51 +0800

    Update Ho035.sh

--- Ho035.sh
+++ Ho035.sh
@@ -1,13 +1,1 @@
 #!/bin/sh
-sudo rm /var/lib/nyzo/production/nodes
-sudo wget -P /var/lib/nyzo/production/ https://raw.githubusercontent.com/junscapital/reload/master/nodes
-sudo rm /home/blockip.sh
-sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
-sudo bash /home/blockip.sh
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
