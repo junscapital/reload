e53e95071d262fddde8a0a379702ae8833b55f5c
junscapital <52300722+junscapital@users.noreply.github.com>
2021-07-06 12:12:29 +0800

    Update Ho035.sh

--- Ho035.sh
+++ Ho035.sh
@@ -1,7 +1,1 @@
 #!/bin/sh
-sudo su
-sudo sed -i '1i\904e18e75bc2d2de-​e8478a536daae44f-​04d571d3830b0bc9-​a7edfa5c6709dac6:18.200.209.25:9444:9446:1605542822106:0:-1' /var/lib/nyzo/production/nodes
-sudo sed -i '1i\3c2b522eab097804-​56c85ce5e799cde5-​67eb9e798b9d8217-​098552f082915ccd:34.240.177.156:9444:9446:1605542822206:0:-1' /var/lib/nyzo/production/nodes
-sudo sed -i '1i\3c2b522eab097804-​56c85ce5e799cde5-​67eb9e798b9d8217-​098552f082915ccd:54.74.26.188:9444:9446:1605542822306:0:-1' /var/lib/nyzo/production/nodes
-sudo sed -i '1i\31051e0b0895b707-​ae97617cf5cb1f4c-​34bcc82e405ee8cf-​305dc258b96947a4:54.170.98.6:9444:9446:1605542822406:0:-1' /var/lib/nyzo/production/nodes
-sudo sed -i '1i\000a96f88bef810b-​e275ddc81c5dbc18-​eeef0345b8644c34-​1770f6815d9ac249:34.240.31.217:9444:9446:1605542822506:0:-1' /var/lib/nyzo/production/nodes
