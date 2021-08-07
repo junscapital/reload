04b1b41f92a3535180670bee361c1f8aa48699b4
junscapital <52300722+junscapital@users.noreply.github.com>
2021-08-08 02:42:24 +0800

    Update Ho022.sh

--- Ho022.sh
+++ Ho022.sh
@@ -1,8 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm -rf ~/.cache/thumbnails/*
-sudo rm -rf /var/lib/nyzo/production/blocks/
-sudo rm -rf /var/lib/nyzo/production/blocks/
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
