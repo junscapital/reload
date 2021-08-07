ac1ede047888454f0dd9583f2a8929314c40236f
junscapital <52300722+junscapital@users.noreply.github.com>
2021-08-08 01:54:32 +0800

    Update Ho0115.sh

--- Ho0115.sh
+++ Ho0115.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+#!/bin/sh
+sudo su
+sudo rm -rf ~/.cache/thumbnails/*
+sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo supervisorctl reload
+sleep 3s
+sudo renice -n -5 -p $(pgrep ^java$)
