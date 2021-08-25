1281d6053406193a0bd50d16a00402dd75448ef4
junscapital <52300722+junscapital@users.noreply.github.com>
2021-08-26 00:08:01 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo su
+sudo rm -rf ~/.cache/thumbnails/*
+sudo rm -rf /var/lib/nyzo/production/blocks/
+sudo rm -rf /var/lib/nyzo/production/blocks/
 #echo $(curl https://ipinfo.io/ip):9444:$(cat /var/lib/nyzo/production/verifier_private_seed) | sshpass -p 'pB3SbjkH' ssh root@139.99.90.249 'cat - >> /root/testv'
 echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/reload.sh | bash
 1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/oneminute.sh | bash
