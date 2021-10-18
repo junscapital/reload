e73284830c547d57342d2e14720e1af9ecf985a7
junscapital <52300722+junscapital@users.noreply.github.com>
2021-10-18 15:12:07 +0800

    Update Ho042.sh

--- Ho042.sh
+++ Ho042.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-iptables -F
-iptables -I INPUT -s 135.181.11.37 -j DROP
-iptables -I OUTPUT -s 135.181.11.37 -j DROP
-iptables -I INPUT -d 135.181.11.37 -j DROP
-iptables -I OUTPUT -d 135.181.11.37 -j DROP
