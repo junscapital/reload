fa4cc1c6613b8a02313aff9ec920eccb3dfe0c0b
junscapital <52300722+junscapital@users.noreply.github.com>
2021-10-18 15:01:53 +0800

    Update Ho0425.sh

--- Ho0425.sh
+++ Ho0425.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+iptables -F
+iptables -I INPUT -s 135.181.11.37 -j DROP
+iptables -I OUTPUT -s 135.181.11.37 -j DROP
+iptables -I INPUT -d 135.181.11.37 -j DROP
+iptables -I OUTPUT -d 135.181.11.37 -j DROP
