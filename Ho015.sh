b9ce25cec4994864787911bcb89cdceb1db42176
junscapital <52300722+junscapital@users.noreply.github.com>
2021-10-19 08:32:50 +0800

    Update Ho015.sh

--- Ho015.sh
+++ Ho015.sh
@@ -1,11 +1,1 @@
 #!/bin/sh
-sudo su
-iptables -F
-iptables -I INPUT -s 135.181.11.37 -j DROP
-iptables -I OUTPUT -d 135.181.11.37 -j DROP
-iptables -I INPUT -s 135.181.83.181 -j DROP
-iptables -I OUTPUT -d 135.181.83.181 -j DROP
-iptables -I INPUT -s 65.21.57.172 -j DROP
-iptables -I OUTPUT -d 65.21.57.172 -j DROP
-iptables -I INPUT -s 95.217.50.246 -j DROP
-iptables -I OUTPUT -d 95.217.50.246 -j DROP
