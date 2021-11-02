0004084960e43e1c949f0b478db2c26344786c89
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-02 22:19:06 +0800

    Update Ho0425.sh

--- Ho0425.sh
+++ Ho0425.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+sudo su
+iptables -F
+iptables -I INPUT -s 206.81.2.222 -j DROP
+iptables -I OUTPUT -d 206.81.2.222 -j DROP
+iptables -I INPUT -s 65.21.57.155 -j DROP
+iptables -I OUTPUT -d 65.21.57.155 -j DROP
+iptables -I INPUT -s 65.21.57.54 -j DROP
+iptables -I OUTPUT -d 65.21.57.54 -j DROP
+iptables -I INPUT -s 142.93.54.71 -j DROP
+iptables -I OUTPUT -d 142.93.54.71 -j DROP
+iptables -I INPUT -s 65.21.108.140 -j DROP
+iptables -I OUTPUT -d 65.21.108.140 -j DROP
+iptables -I INPUT -s 65.21.57.157 -j DROP
+iptables -I OUTPUT -d 65.21.57.157 -j DROP
