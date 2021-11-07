7dfc47167ec148140599ca4ea85651ec64d49e09
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-07 09:53:03 +0800

    Update Ho013.sh

--- Ho013.sh
+++ Ho013.sh
@@ -1,28 +1,6 @@
 #!/bin/sh
 sudo su
 iptables -F
-iptables -I INPUT -s 65.21.109.206 -j DROP
-iptables -I OUTPUT -d 65.21.109.206 -j DROP
-iptables -I INPUT -s 65.108.89.188 -j DROP
-iptables -I OUTPUT -d 65.108.89.188 -j DROP
-iptables -I INPUT -s 65.21.57.174 -j DROP
-iptables -I OUTPUT -d 65.21.57.174 -j DROP
-iptables -I INPUT -s 65.108.90.241 -j DROP
-iptables -I OUTPUT -d 65.108.90.241 -j DROP
-iptables -I INPUT -s 65.21.56.95 -j DROP
-iptables -I OUTPUT -d 65.21.56.95 -j DROP
-iptables -I INPUT -s 65.108.95.239 -j DROP
-iptables -I OUTPUT -d 65.108.95.239 -j DROP
-iptables -I INPUT -s 135.181.82.157 -j DROP
-iptables -I OUTPUT -d 135.181.82.157 -j DROP
-iptables -I INPUT -s 95.216.147.47 -j DROP
-iptables -I OUTPUT -d 95.216.147.47 -j DROP
-iptables -I INPUT -s 65.21.57.178 -j DROP
-iptables -I OUTPUT -d 65.21.57.178 -j DROP
-iptables -I INPUT -s 65.108.90.163 -j DROP
-iptables -I OUTPUT -d 65.108.90.163 -j DROP
-iptables -I INPUT -s 95.217.177.182 -j DROP
-iptables -I OUTPUT -d 95.217.177.182 -j DROP
 iptables -I INPUT -s 65.21.110.223 -j DROP
 iptables -I OUTPUT -d 65.21.110.223 -j DROP
 iptables -I INPUT -s 135.125.233.31 -j DROP
