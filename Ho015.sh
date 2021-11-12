013d53fe4219ca3e793aa0647f0418f4d854a06d
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-12 19:24:57 +0800

    Update Ho015.sh

--- Ho015.sh
+++ Ho015.sh
@@ -1,13 +1,1 @@
 #!/bin/sh
-sudo su
-ipset add queue 70.35.195.112
-ipset add queue 87.106.193.53
-ipset add queue 217.160.44.166
-ipset add queue 104.192.6.141
-ipset add queue 108.175.11.225
-ipset add queue 217.160.43.100
-ipset add queue 108.175.2.155
-ipset add queue 82.165.123.203
-ipset add queue 82.165.123.203
-ipset add queue 82.165.30.118
-sudo rm /home/blockip.sh
