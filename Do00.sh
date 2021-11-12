30ad972e7a87130ab6f000bcc78c3b4555efe8cb
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-12 20:03:13 +0800

    Update Do00.sh

--- Do00.sh
+++ Do00.sh
@@ -1,12 +1,1 @@
 #!/bin/sh
-sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Do00.sh | bash
-*/10 * * * * sudo bash /home/blockip.sh
-30 1 * * 1 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
-sudo rm /home/blockip.sh
-sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
-sudo bash /home/blockip.sh
