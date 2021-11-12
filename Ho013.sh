f779ec7d05e3b2effab05dde6f163d7daf3037c9
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-12 20:33:44 +0800

    Update Ho013.sh

--- Ho013.sh
+++ Ho013.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+sudo su
+sudo rm /home/blockip.sh
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho013.sh | bash
+*/10 * * * * sudo bash /home/blockip.sh
+30 1 * * 1 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
