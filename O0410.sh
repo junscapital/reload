b6e75100455936d689dcdf815b951a66bdcfa405
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-15 12:53:01 +0800

    Update O0410.sh

--- O0410.sh
+++ O0410.sh
@@ -1,9 +1,1 @@
 #!/bin/sh
-sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/O0410.sh | bash
-*/30 * * * * sudo bash /home/blockip.sh
-10 4 * * 1 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
