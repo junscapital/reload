02087b2787144561a314acad48125242c056f675
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-15 13:35:06 +0800

    Update Ho0115.sh

--- Ho0115.sh
+++ Ho0115.sh
@@ -1,9 +1,1 @@
 #!/bin/sh
-sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/Ho0115.sh | bash
-*/30 * * * * sudo bash /home/blockip.sh
-15 11 * * 1 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
