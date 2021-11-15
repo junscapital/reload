6e8e2efe8a842c182ad42df762f932389c24f86c
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-15 12:52:20 +0800

    Update O1050.sh

--- O1050.sh
+++ O1050.sh
@@ -1,9 +1,1 @@
 #!/bin/sh
-sudo su
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/O1050.sh | bash
-*/30 * * * * sudo bash /home/blockip.sh
-50 10 * * 1 sudo rm /home/blockip.sh; sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
