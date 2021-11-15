a3b1a60a3bbcc7f8b822d5adf905b9ac627f84f7
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-15 20:33:58 +0800

    Update inreload.sh

--- inreload.sh
+++ inreload.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+sudo su
+ipset create cycle hash:ip
+ipset create queue hash:ip
+iptables -I INPUT -m set --match-set cycle src -j DROP  
+iptables -I OUTPUT -m set --match-set cycle src -j DROP  
+iptables -I INPUT -m set --match-set queue src -j DROP  
+iptables -I OUTPUT -m set --match-set queue src -j DROP 
+curl https://raw.githubusercontent.com/junscapital/reload/master/banip.sh | bash
+sudo rm /home/blockip.sh
+sudo wget -P /home/ https://raw.githubusercontent.com/junscapital/reload/master/blockip.sh
+sudo bash /home/blockip.sh
 cd /home/ubuntu/nyzoVerifier
 sudo git reset --hard
 sudo git pull origin master
