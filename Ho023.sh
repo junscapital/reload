aa3dd34dc4b642840ec14588a8db49472596b061
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-22 20:42:14 +0800

    Update Ho023.sh

--- Ho023.sh
+++ Ho023.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+sudo su
+ipset del cycle 135.181.65.0/24
+ipset del cycle 135.181.11.0/24
+ipset del cycle 135.181.65.0/24
+ipset del cycle 144.217.235.0/24
+ipset del cycle 159.223.8.0/24
+ipset del cycle 167.114.88.0/24
+ipset del cycle 51.195.166.0/24
+ipset del cycle 95.217.50.0/24
