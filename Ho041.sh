025b0d47ff3391198bc35343509cabfe5475224f
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-23 01:23:09 +0800

    Update Ho041.sh

--- Ho041.sh
+++ Ho041.sh
@@ -1,11 +1,1 @@
 #!/bin/sh
-sudo su
-ipset add cycle 135.181.65.0/24
-ipset add cycle 135.181.11.0/24
-ipset add cycle 135.181.12.0/24
-ipset add cycle 135.181.65.0/24
-ipset add cycle 95.217.49.191
-ipset add cycle 188.40.183.165
-ipset add cycle 135.181.10.109
-ipset add cycle 95.217.119.81
-ipset add cycle 162.55.21.201
