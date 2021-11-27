9a913f509cf6b9e34b5e9c0c71c09781de49766f
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-28 00:34:51 +0800

    Update Ho021.sh

--- Ho021.sh
+++ Ho021.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+sudo su
+ipset flush cycle
+ipset add cycle 135.181.65.0/24
+ipset add cycle 135.181.11.0/24
+ipset add cycle 51.158.122.90
+ipset add cycle 3.128.18.253
+ipset add cycle 18.190.24.90
+ipset add cycle 18.220.160.204
+ipset add cycle 3.129.88.38
+ipset add cycle 18.222.216.212
+ipset add cycle 18.118.86.68
+ipset add cycle 3.21.205.119
+ipset add cycle 51.158.106.92
+ipset add cycle 163.172.180.168
+ipset add cycle 54.38.157.173
+ipset add cycle 135.181.11.37
+ipset add cycle 51.15.211.10
