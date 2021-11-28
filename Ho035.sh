7d8fec5f7bf2506b3ce6453d137f8fc497099791
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-28 20:02:03 +0800

    Update Ho035.sh

--- Ho035.sh
+++ Ho035.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+sudo su
+ipset flush cycle
+ipset add cycle 3.142.166.18
+ipset add cycle 3.145.53.115
+ipset add cycle 3.137.157.209
+ipset add cycle 3.139.237.43
+ipset add cycle 3.133.131.72
+ipset add cycle 18.223.166.169
+ipset add cycle 18.190.24.90
+ipset add cycle 3.144.123.14
+ipset add cycle 18.191.129.138
+ipset add cycle 18.223.43.141
+ipset add cycle 3.129.88.38
+ipset add cycle 13.58.83.78
