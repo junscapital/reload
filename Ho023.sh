d7bb1ebf2135d52b2732077ef7915f635e280d12
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-28 13:32:22 +0800

    Update Ho023.sh

--- Ho023.sh
+++ Ho023.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+sudo su
+ipset flush cycle
+ipset add cycle 135.181.65.0/24
+ipset add cycle 3.129.217.104
+ipset add cycle 3.145.67.213
+ipset add cycle 51.158.122.90
+ipset add cycle 3.129.88.38
+ipset add cycle 13.58.83.78
+ipset add cycle 18.118.86.68
+ipset add cycle 18.222.168.89
+ipset add cycle 18.216.190.185
+ipset add cycle 18.190.24.90
+ipset add cycle 3.144.123.14
+ipset add cycle 3.145.64.208
+ipset add cycle 51.158.111.217
+ipset add cycle 18.221.27.11
+ipset add cycle 13.59.213.179
+ipset add cycle 3.129.45.22
+ipset add cycle 18.117.142.108
+ipset add cycle 18.218.4.60
+ipset add cycle 18.221.95.86
+ipset add cycle 51.15.225.240
