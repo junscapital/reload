956e54a0073cd728c629701a4d7a5f933db39408
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-29 11:55:03 +0800

    Update Ho0215.sh

--- Ho0215.sh
+++ Ho0215.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+sudo su
+ipset flush cycle
+ipset add cycle 3.143.25.254
+ipset add cycle 3.22.242.165
+ipset add cycle 3.145.67.71
+ipset add cycle 3.139.86.84
+ipset add cycle 3.17.76.114
+ipset add cycle 3.129.253.185
+ipset add cycle 18.117.75.75
+ipset add cycle 18.116.162.231
