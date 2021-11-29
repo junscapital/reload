b1605bc114592c120a189035ca80c84014de3cb9
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-29 22:05:22 +0800

    Update O1010.sh

--- O1010.sh
+++ O1010.sh
@@ -1,8 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm /home/blockip.sh
-echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
-1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/O1010.sh | bash
- " >> myverifier
-sudo crontab myverifier
-rm myverifier
