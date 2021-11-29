f03037ffdab03f76eb9e81bc1dd78ac894fc9cb5
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-29 12:36:00 +0800

    Update O0950.sh

--- O0950.sh
+++ O0950.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+sudo su
+ipset add queue 138.124.180.212
+ipset add queue 193.203.203.117
+ipset add queue 193.203.203.116
+ipset add queue 138.124.183.57
+ipset add queue 45.67.229.231
+ipset add queue 45.133.216.19
+ipset add queue 194.156.99.54
+ipset add queue 194.156.99.51
+ipset add queue 45.153.229.244
+ipset add queue 45.67.229.113
+ipset add queue 45.144.31.19
+ipset add queue 45.153.229.241
+ipset add queue 45.142.215.167
+ipset add queue 45.67.229.54
+ipset add queue 193.203.203.239
+ipset add queue 70.37.94.95
+ipset add queue 40.84.184.205
+ipset add queue 144.91.100.76
+ipset add queue 137.116.34.176
