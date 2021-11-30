92e3f9a75935660a6455d4a1609725020e8ff3ec
junscapital <52300722+junscapital@users.noreply.github.com>
2021-11-30 09:33:19 +0800

    Update Ho013.sh

--- Ho013.sh
+++ Ho013.sh
@@ -1,13 +1,1 @@
 #!/bin/sh
-sudo su
-ipset flush cycle
-ipset add cycle 3.12.152.36
-ipset add cycle 3.15.235.77
-ipset add cycle 3.17.188.247
-ipset add cycle 3.129.253.185
-ipset add cycle 18.218.123.239
-ipset add cycle 3.145.67.71
-ipset add cycle 18.191.164.213
-ipset add cycle 3.137.198.243
-ipset add cycle 18.118.198.254
-ipset add cycle 3.143.25.254
