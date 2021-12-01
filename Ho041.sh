e3ebda76780f8141c796c5bdf8ac7623a7128cb9
junscapital <52300722+junscapital@users.noreply.github.com>
2021-12-01 23:00:08 +0800

    Update Ho041.sh

--- Ho041.sh
+++ Ho041.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+sudo su
+ipset flush cycle
+ipset add cycle 3.138.33.255
+ipset add cycle 18.190.153.248
+ipset add cycle 52.14.56.168
+ipset add cycle 52.15.101.166
+ipset add cycle 18.223.43.21
+ipset add cycle 161.35.89.207
