efaad20ca438b1b50892a8f09db39890e415e0b9
junscapital <52300722+junscapital@users.noreply.github.com>
2021-12-12 11:55:10 +0800

    Update Ho042.sh

--- Ho042.sh
+++ Ho042.sh
@@ -1,1 +1,26 @@
 #!/bin/sh
+sudo su
+ipset flush cycle
+ipset add cycle 18.222.184.162
+ipset add cycle 18.119.29.38
+ipset add cycle 161.35.89.207
+ipset add cycle 3.138.33.255
+ipset add cycle 52.14.56.168
+ipset add cycle 18.190.153.248
+ipset add cycle 18.223.43.21
+ipset add cycle 52.15.101.166
+ipset add cycle 95.217.50.240
+ipset add cycle 95.217.50.241
+ipset add cycle 95.217.50.242
+ipset add cycle 95.217.50.243
+ipset add cycle 95.217.50.244
+ipset add cycle 95.217.50.245
+ipset add cycle 95.217.50.246
+ipset add cycle 95.217.50.247
+ipset add cycle 95.217.50.248
+ipset add cycle 95.217.50.249
+ipset add cycle 95.217.50.250
+ipset add cycle 95.217.50.251
+ipset add cycle 95.217.50.252
+ipset add cycle 95.217.50.253
+ipset add cycle 95.217.50.254
