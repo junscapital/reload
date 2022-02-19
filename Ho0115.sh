47785eb6d75330399182bc2fd1354241a417415f
junscapital <52300722+junscapital@users.noreply.github.com>
2022-02-19 18:21:44 +0800

    Update Ho0115.sh

--- Ho0115.sh
+++ Ho0115.sh
@@ -1,12 +1,1 @@
 #!/bin/sh
-sudo su
-sudo rm -rf ~/.cache/thumbnails/*
-sudo rm -rf /home/ubuntu/nyzoVerifier 
-sudo rm -rf /root/nyzoVerifier
-cd /home/ubuntu/
-git clone https://github.com/Open-Nyzo/nyzoVerifier.git
-cd /home/ubuntu/nyzoVerifier
-sudo ./gradlew build
-sudo supervisorctl reload 
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
