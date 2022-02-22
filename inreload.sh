032141cc39e5f1bbac4b9bac04a9fba6d621e1f2
junscapital <52300722+junscapital@users.noreply.github.com>
2022-02-23 05:03:10 +0800

    Update inreload.sh

--- inreload.sh
+++ inreload.sh
@@ -1,15 +1,1 @@
 #!/bin/sh
-sudo su
-ipset create cycle hash:ip
-ipset create queue hash:ip
-iptables -I INPUT -m set --match-set cycle src -j DROP  
-iptables -I OUTPUT -m set --match-set cycle src -j DROP  
-iptables -I INPUT -m set --match-set queue src -j DROP  
-iptables -I OUTPUT -m set --match-set queue src -j DROP 
-cd /home/ubuntu/nyzoVerifier
-sudo git reset --hard
-sudo git pull origin master
-sudo ./gradlew build
-sudo supervisorctl reload
-sleep 3s
-sudo renice -n -5 -p $(pgrep ^java$)
